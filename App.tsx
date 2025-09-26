26e90b5014bca863f93958b804146909265b7f64
Tanmay Jain <tanmayjain4477@gmail.com>
2025-09-26 20:00:06 +0530

    initial commit with nativewind setup

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      {/* <ScreenContent title="Home" path="App.tsx"></ScreenContent> */}
+      <View>
+        <Text className='bg-red-500 mt-20'>
+          Hi, hello how are you
+        </Text>
+        <Text className='bg-yellow-500'>
+          See mee
+        </Text>
+
+      </View>
     </>
   );
 }
